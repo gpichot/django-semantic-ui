4537e9fd0dda0d0e36cd0cbb82433dffb8c62c3b
Gabriel Pichot <gabriel.pichot@gmail.com>
2015-07-20 00:48:08 +0200

    Added templatetags

--- semantic_ui/templatetags/semantic_ui.py
+++ semantic_ui/templatetags/semantic_ui.py
@@ -12,6 +12,7 @@ def semantic_header(
             icon=None,
             inverted=False,
             divider=False,
+            level=1,
         ):
     """
     A simple tag that outputs a semantic ui header.
@@ -28,11 +29,34 @@ def semantic_header(
         'icon': icon,
         'inverted': inverted,
         'divider': divider,
+        'level': level,
     }))
 
 
 @register.simple_tag
-def semantic_icon(icon):
+def semantic_icon(icon, klass=''):
     return '<i class="{icon} icon"></i>'.format(
         icon=icon,
     )
+
+
+@register.simple_tag
+def semantic_button(content, icon=None, klass=None):
+    template = loader.get_template('semantic-ui/button.html')
+    return template.render(Context({
+        'content': content,
+        'icon': icon,
+        'class': klass,
+    }))
+
+
+@register.simple_tag
+def semantic_button_link(url, content='', icon=None, klass=None, as_popup=True):
+    template = loader.get_template('semantic-ui/button_link.html')
+    return template.render(Context({
+        'url': url,
+        'content': content,
+        'icon': icon,
+        'class': klass,
+        'as_popup': True,
+    }))
